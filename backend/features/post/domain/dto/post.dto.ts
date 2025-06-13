f40436e95ac21203be516a6f100f1c7c52325dbc
Youngjae <2015fromjeki@gmail.com>
2025-06-14 09:40:41 +1000

    Refact: Update post.dto

--- backend/features/post/domain/dto/post.dto.ts
+++ backend/features/post/domain/dto/post.dto.ts
@@ -2,6 +2,7 @@ export interface CreatePostDTO {
   title: string;
   content: string;
   image_url?: string;
+  reward_amount?: number;
   user_id: string;
 }
 // UPDATE POST DTO INTERFACE
@@ -10,5 +11,7 @@ export interface UpdatePostDTO {
   updatedTitle?: string;
   updatedContent?: string;
   updatedImageUrl?: string;
+  reward_amount?: number;
+  is_found?: boolean;
   updated_at?: Date;
 }
