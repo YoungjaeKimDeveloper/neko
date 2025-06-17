e1e4b418dd2ca7fdbb8ad928c9bf535af70eac9c
Youngjae <2015fromjeki@gmail.com>
2025-06-17 19:18:39 +1000

    Refact: create const message

--- backend/lib/utils/constants/http-status.ts
+++ backend/lib/utils/constants/http-status.ts
@@ -1,8 +1,10 @@
 /* 
-enum-like HTTP constant
+
+  enum-like HTTP constant
+
 */
 
-export const HTTP = {
+export const RESPONSE_HTTP = {
   OK: 200,
   CREATED: 201,
   BAD_REQUEST: 400,
---
+++ backend/lib/utils/constants/messages.ts
@@ -0,0 +1,14 @@
+/*
+
+    enum-like Response Messages
+
+*/
+
+export const RESPONSE_MESSAGES = {
+  UPDATE: "Data updated successfully",
+  DELETE: "Data deleted successfully",
+  BAD_REQUEST: "Bad request",
+  UNAUTHORIZED: "Unauthorized",
+  NOT_FOUND: "Not found",
+  INTERNAL: "Server error",
+};
