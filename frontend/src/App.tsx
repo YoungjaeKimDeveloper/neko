1a752e9a9790e2e863436b615574ac66c8b41edf
Youngjae <2015fromjeki@gmail.com>
2025-06-24 08:10:15 +1000

    Feat: Implement axios instance

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,14 +1,17 @@
 import { Route, Routes } from "react-router-dom";
+
+
 // Components
 import Navbar from "./shared/components/Navbar";
 // Pages
 import Landingpage from "./shared/pages/mobile/LandingPage";
-
 // Auth
 import AuthLoginPage from "./features/auth/pages/AuthLoginPage";
 import AuthSignUpPage from "./features/auth/pages/AuthSignUpPage";
 
 const App = () => {
+  
+  // Render
   return (
     <div>
       <Navbar />
