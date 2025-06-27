9f66777e617d7106fc1f339033f81820f68d3958
Youngjae <2015fromjeki@gmail.com>
2025-06-27 12:22:33 +1000

    Feat : Finalised CreatePostPage

--- frontend/src/features/post/pages/common/CreatePostPage.tsx
+++ frontend/src/features/post/pages/common/CreatePostPage.tsx
@@ -2,10 +2,12 @@
 /*
 
     Create Post page - common(Mobile + Desktop)
-
+    Todo 
+      1. Show error message()
+      1. Send the form to the backend()
 */
 
-import { useForm } from "react-hook-form";
+import { useForm, type SubmitHandler } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
 import PostInput from "../../components/common/PostInput";
 import { AuthDesktopSidebar } from "../../../auth/components/desktop/AuthDesktopSidebar";
@@ -14,12 +16,18 @@ import type { ImageListType } from "react-images-uploading";
 import { useEffect, useState } from "react";
 import { PostSchema, type PostFormValues } from "../../schema/postSchema";
 import ImageUploader from "../../../../shared/components/ImageUploader";
-
+import { axiosInstance } from "../../../../shared/api/axios";
+import { RESPONSE_HTTP } from "../../../../../../shared/constants/http-status";
+import { errorLogV2 } from "../../../../../../shared/error/error.log";
+import toast from "react-hot-toast";
+import type { ResponseDTO } from "../../../../../../shared/dto/common/response.dto";
+import { useNavigate } from "react-router-dom";
 // Schema - Runtime
 
 // Inferred Type - Complie
 // Component
 const CreatePostPage = () => {
+  const navigate = useNavigate();
   // React hook-form (RHF)
   const {
     register,
@@ -36,15 +44,18 @@ const CreatePostPage = () => {
     // Runtime Checker(Resolver)
     resolver: zodResolver(PostSchema),
   });
+  // Track the number of description
+  const [description, setdescription] = useState<number>(0);
+
+  // Image Tracker[S] -------------
   // Todo - Double check
   // As image uploder is external libary,it is impossible to track using input,
   // So, track the value, manually.
   useEffect(() => {
-    register("images");
+    register("image_urls");
   }, [register]);
-
   // Add "images" to register
-  register("images");
+  register("image_urls");
   //   Save images in array
   const [images, setImages] = useState<ImageListType>([]);
   // onChange method
@@ -54,9 +65,31 @@ const CreatePostPage = () => {
     // Track the image value manually
     // images[key] : value[imageList.map((img)=>img.file)]
     setValue(
-      "images",
+      "image_urls",
       imageList.map((img) => img.data_url) // base64
     );
+  };
+  // Image Tracker[E] ------------------
+  const onSubmit: SubmitHandler<PostFormValues> = async (
+    data: PostFormValues
+  ) => {
+    try {
+      const result = await axiosInstance.post<ResponseDTO>("/posts", data);
+      if (result.status !== RESPONSE_HTTP.CREATED) {
+        toast.error(`Failed to create new post ${result.data?.message}`);
+      }
+      await navigate("/home");
+      // Tood -Invalidate queirs
+      toast.success("Post created✅");
+    } catch (error) {
+      errorLogV2({
+        file: "CreatePostPage",
+        function: "onSutmib",
+        error: error,
+      });
+      toast.error("Failed to post");
+      return;
+    }
   };
   // BUILD UI
   return (
@@ -64,7 +97,10 @@ const CreatePostPage = () => {
       {/* Left Sidebar */}
       <AuthDesktopSidebar />
       {/* Right - main */}
-      <div className="flex flex-col items-start w-screen  gap-y-4">
+      <form
+        className="flex flex-col items-start w-screen  gap-y-4"
+        onSubmit={handleSubmit(onSubmit)}
+      >
         {/* Image Preview */}
         <div
           className={`w-[100%] mx-auto h-20 mt-10 max-w-[600px]  flex flex-col justify-between`}
@@ -72,6 +108,7 @@ const CreatePostPage = () => {
           <p>Create post</p>
           {/* Image Uploader */}
           <ImageUploader images={images} onChange={onChange} />
+          {errors.image_urls?.message}
         </div>
         {/* Title */}
         <PostInput
@@ -79,6 +116,7 @@ const CreatePostPage = () => {
           hintText="Please help me find the cat"
           numberOfLetters={10}
           register={register("title")}
+          errorMessage={errors.title?.message}
         />
         {/* Text area - Description */}
         <div className="w-[100%] mx-auto h-[200px] mt-5 max-w-[600px]">
@@ -88,18 +126,22 @@ const CreatePostPage = () => {
               <div className="flex flex-col w-full h-full">
                 <p>Description</p>
                 <textarea
-                  {...register("description")}
-                  maxLength={100}
+                  {...register("content")}
+                  maxLength={300}
                   placeholder="I lost my cute cat near Townhall station"
                   className={
                     "input shadow-postInput w-full  mt-3 font-content pl-[10px] h-full py-2 resize-none"
                   }
+                  onChange={(e) => setdescription(e.target.value.length)}
                 />
               </div>
               {/* World counter */}
               <div className="flex justify-end w-full ">
                 <div className="flex items-end">
-                  <p className="text-hintText">0/{100}</p>
+                  <p>{errors.content?.message} </p>
+                  <p className="text-hintText">
+                    {description}/{300}
+                  </p>
                 </div>
               </div>
             </div>
@@ -109,16 +151,22 @@ const CreatePostPage = () => {
           title="Reward amount"
           hintText="$0"
           numberOfLetters={10}
-          register={register("rewardAmount")}
+          register={register("reward_amount")}
+          errorMessage={errors.reward_amount?.message}
         />
         <PostInput
           title="Location"
           hintText="Townhall station"
-          numberOfLetters={20}
+          numberOfLetters={10}
           register={register("location")}
+          errorMessage={errors.location?.message}
         />
-        <MainButton text="Post" style="w-[40%] mt-5 " />
-      </div>
+        {isSubmitting ? (
+          <p>Loading...</p>
+        ) : (
+          <MainButton text="Post" style="w-[40%] mt-5 " />
+        )}
+      </form>
     </div>
   );
 };
--- frontend/src/features/post/schema/postSchema.tsx
+++ frontend/src/features/post/schema/postSchema.tsx
@@ -7,19 +7,23 @@ import { z } from "zod";
 
 // Post Schema - Runtime
 export const PostSchema = z.object({
-  title: z.string().min(5, { message: "Title should be at least 5 letters" }),
-  images: z
+  title: z
+    .string()
+    .min(5, { message: "Description is too short (min: 3 letters)" })
+    .max(10, { message: "Location is too long (max: 10 letters)" }),
+  image_urls: z
     .array(z.any())
     .min(1, { message: "At least 1 image is required" })
     .max(5, { message: "At most 5 images allowed" }),
-  description: z
+  content: z
     .string()
-    .min(10, { message: "Description should be at least 10 letters" }),
-  rewardAmount: z.coerce.number(),
+    .min(10, { message: "Description is too short (min: 10 letters)" })
+    .max(300, { message: "Description is too long (max: 300 letters)" }),
+  reward_amount: z.coerce.number(),
   location: z
     .string()
-    .min(3, { message: "Location should be at least 3 letters" })
-    .max(10, { message: "Location is too long (max:15 letters" }),
+    .min(3, { message: "Location is too short (min: 3 letters)" })
+    .max(10, { message: "Location is too long (max: 15 letters)" }),
 });
 
 // Inferred type for Post - Compile
