0db0f35d233aefa0cf6c34d19867688a84d515af
Youngjae <2015fromjeki@gmail.com>
2025-07-02 00:29:16 +1000

    Feat: Finalised ProfilePage

--- frontend/src/features/profile/pages/ProfilePage.tsx
+++ frontend/src/features/profile/pages/ProfilePage.tsx
@@ -5,14 +5,16 @@
 
 */
 
-import { useQueryClient } from "@tanstack/react-query";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 import UserProfilePicture from "../../../shared/components/UserProfilePicture";
 import { AuthDesktopSidebar } from "../../auth/components/desktop/AuthDesktopSidebar";
 import ProfileInput from "../components/ProfileInput";
-import { useState } from "react";
+import React, { useState } from "react";
 
 import { format } from "date-fns";
 import MainButton from "../../../shared/components/MainButton";
+import { axiosInstance } from "../../../shared/api/axios";
+import toast from "react-hot-toast";
 
 type ProfilePage = {
   user_profile: string;
@@ -20,17 +22,51 @@ type ProfilePage = {
   user_name: string;
   location: string;
   created_at: Date;
+  user_profile_image: string;
 };
 // Components
 const ProfilePage = () => {
   const queryClient = useQueryClient();
   const currentUser = queryClient.getQueryData<ProfilePage>(["authUser"]);
   // Profile Image Url
-  const [uploadImageFile, setUploadImageFile] = useState<File | null>(null);
+  const [uploadImageFile, setUploadImageFile] = useState<File>();
   const [previewUrl, setPreviewUrl] = useState<string | null>(null);
   const [updatedLocation, setUpdatedLocation] = useState<string>(
     currentUser!.location
   );
+
+  // Helper Function convert the file to Base 64
+  const fileToBase64 = (file: File): Promise<string> =>
+    new Promise((resolve, reject) => {
+      const reader = new FileReader();
+      reader.onloadend = () => resolve(reader.result as string);
+      reader.onerror = reject;
+      reader.readAsDataURL(file);
+    });
+  // convert the data to base 64
+  let base64 = currentUser?.user_profile_image;
+  const { mutate: updateProfile, isLoading: isUpdating } = useMutation({
+    mutationFn: async () => {
+      // if there is something to update
+      if (uploadImageFile) {
+        base64 = await fileToBase64(uploadImageFile);
+        // throw new Error("Image file is required");
+      }
+      const result = await axiosInstance.put("/profile/", {
+        updated_profile_image_url: base64,
+        updated_location: updatedLocation,
+      });
+      return result;
+    },
+    onSuccess: () => {
+      toast.success("User Profile updated successfully");
+    },
+    onError: (error) => {
+      if (error instanceof Error) {
+        toast.error(`Failed to update Profile ${error.message}`);
+      }
+    },
+  });
   if (!currentUser) {
     return null;
   }
@@ -41,6 +77,7 @@ const ProfilePage = () => {
     location,
     user_name: userName,
     user_profile: userProfile,
+    user_profile_image,
   } = currentUser;
 
   // Side Effect
@@ -59,6 +96,11 @@ const ProfilePage = () => {
   console.log("UploadedFile", uploadImageFile);
   console.log("New Location", updatedLocation);
   console.log("New Preview Image Url", previewUrl);
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    updateProfile();
+  };
+  console.log(currentUser);
   // BUILD UI
   return (
     <div className="flex">
@@ -70,7 +112,10 @@ const ProfilePage = () => {
           {/* Component Container */}
           <div className="flex flex-col items-center h-full bg-gray-400 max-w-[800px] mx-auto">
             {/* Heading + Image */}
-            <form className="mt-5 flex flex-col items-center justify-between w-full h-fit">
+            <form
+              className="mt-5 flex flex-col items-center justify-between w-full h-fit"
+              onSubmit={(e) => handleSubmit(e)}
+            >
               {/* Profile */}
 
               {/* Profile image */}
@@ -78,7 +123,7 @@ const ProfilePage = () => {
                 <h3 className="text-2xl font-bold tracking-wider">Profile</h3>
 
                 <UserProfilePicture
-                  imageSrc={previewUrl ?? userProfile}
+                  imageSrc={previewUrl ?? user_profile_image}
                   imageSize="size-20"
                   isEditable={true}
                   onImageChange={handleImageChange}
@@ -113,7 +158,7 @@ const ProfilePage = () => {
                 />
               </div>
               <div className="mt-10">
-                <MainButton text="Save" type="submit" />
+                <MainButton text="Save" type="submit" isLoading={isUpdating} />
               </div>
             </form>
           </div>
